cb9af4ae580c9f56db81cf56066de424e73697e9
uuufly-coder <lanlianhualw@outlook.com>
2023-05-10 20:51:14 +0800

    对象 DI

--- src/main/java/com/Person.java
+++ src/main/java/com/Person.java
@@ -1,5 +1,20 @@
 package com;
 
 public class Person {
+    Integer id;
 
+    public Person() {
+    }
+
+    public Person(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/UserDaoImpl.java
+++ src/main/java/com/UserDaoImpl.java
@@ -2,6 +2,12 @@ package com;
 
 public class UserDaoImpl implements UserDao {
 
+    private Person person;
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
     public void say() {
         System.out.println("run");
     }
