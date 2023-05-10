3eb9b70a05d1212e27098a2775b9330d855b1009
uuufly-coder <lanlianhualw@outlook.com>
2023-05-10 13:31:31 +0800

    first commit

---
+++ src/main/java/com/User.java
@@ -0,0 +1,12 @@
+package com;
+
+public class User {
+
+    public User() {
+        System.out.println("null construct");
+    }
+
+    public void s() {
+        System.out.println("function already");
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,20 @@
+import com.User;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+    private Logger logger = LoggerFactory.getLogger(User.class);
+
+    @Test
+    public void userTest() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        User u = (User) ac.getBean("User");
+        u.s();
+        logger.info("success");
+    }
+
+}
