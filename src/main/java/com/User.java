7a346a911cff28cd1832192176cef9e6243de665
uuufly-coder <lanlianhualw@outlook.com>
2023-05-10 14:23:24 +0800

    Update User.java

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -5,7 +5,9 @@ public class User {
     public User() {
         System.out.println("null construct");
     }
-
+    
+    public string name;
+    
     public void say() {
         System.out.println("function already");
     }
