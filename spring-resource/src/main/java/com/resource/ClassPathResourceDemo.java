5137f284dc3cf4697c682cd4115fad32d882ee7d
uuufly-coder <lanlianhualw@outlook.com>
2023-05-31 16:53:23 +0800

    resource

---
+++ spring-resource/src/main/java/com/resource/ClassPathResourceDemo.java
@@ -0,0 +1,30 @@
+package com.resource;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+
+// 访问类加载路径下的资源
+public class ClassPathResourceDemo {
+
+    public static void loadAndReadUrlResource(String path) throws Exception{
+        // 创建一个 Resource 对象
+        ClassPathResource resource = new ClassPathResource(path);
+        // 获取文件名
+        System.out.println("resource.getFileName = " + resource.getFilename());
+        // 获取文件描述
+        System.out.println("resource.getDescription = " + resource.getDescription());
+        //获取文件内容
+        InputStream in = resource.getInputStream();
+        byte[] b = new byte[1024];
+        while(in.read(b)!=-1) {
+            System.out.println(new String(b));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        loadAndReadUrlResource("test.txt");
+    }
+
+
+}
